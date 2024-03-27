538103efc142a3d8a3dd0c2cb01b3e118154400f
Moixime <118693600+Moixime@users.noreply.github.com>
2024-03-27 16:08:32 +0100

    Update connexion et interaction avec bdd fonctionnelle
    -connexion(url,token,org)
    -writeData(client,bucket,org)
    -readData(client,org)
    -affiche_res(result)

--- connexion.py
+++ connexion.py
@@ -1,66 +1,44 @@
-import influxdb_client as influxdb
-from sshtunnel import SSHTunnelForwarder
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+import datetime
 
-# Informations de connexion au serveur
-url = "192.168.141.107"
-ssh_port = 22
-login="root"
-mdp="password"
-#token = "U3lsdmFpbk1vbnRhZ255RXN0VW5DaGFtcGlvbl9Gb3JtYXRpb25Mb1JhV0FOX1VuaXZfU2F2b2llXzIwMjMhCg=="
-#org = "training-usmb"
-#bucket = "iot-platform"
 
-influxdb_address = 'localhost'
-influxdb_port = 8086
-influxdb_username = 'admin'
-influxdb_password = 'univ-lorawan'
-influxdb_bucket = 'iot-platform'
+host = "http://192.168.170.61:81"
+org = "training-usmb"
+token = "U3lsdmFpbk1vbnRhZ255RXN0VW5DaGFtcGlvbl9Gb3JtYXRpb25Mb1JhV0FOX1VuaXZfU2F2b2llXzIwMjMhCg=="
+bucket="iot-platform"
 
-def connexion_bdd(url, ssh_port, login, mdp, influxdb_address, influxdb_port, influxdb_username, influxdb_password, influxdb_bucket):
+
+def connexion(url,token,org):
+    client = influxdb_client.InfluxDBClient(url,token,org)
+    return client
+
+
+def writeData(client, bucket,org):
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    data = influxdb_client.Point("Temperature").tag(key="Admin",value="Maxime").field(field="Salle-Test", value=20).time(time=datetime.datetime.utcnow())
+    write_api.write(bucket=bucket, org=org, record=data)
+
+def readData(client,org):
+    query_api = client.query_api()
+    query = 'from(bucket: "iot-platform")|> range(start: -1h)|> filter(fn:(r) => r._field == "Salle-Test")'
+    result = query_api.query(org=org, query=query)
+    return result
+
+def affiche_res(result):
+    results = []
+    for table in result:
+        for record in table.records:
+            results.append((record.get_field(), record.get_value()))
+    print(results)
+
+def close(client):
+    client.close()
     
-
-    try:
-        # Création d'un tunnel SSH vers le serveur
-        with SSHTunnelForwarder(
-            (url, ssh_port),
-            ssh_username=login,
-            ssh_password=mdp,
-            remote_bind_address=(influxdb_address, influxdb_port),
-        ) as tunnel:
-            print("Tunnel SSH établi avec succès.")
-
-            # Connexion à la base de données InfluxDB à travers le tunnel SSH
-            client = influxdb.InfluxDBClient(
-                url=' http://localhost:8086',
-                token='U3lsdmFpbk1vbnRhZ255RXN0VW5DaGFtcGlvbl9Gb3JtYXRpb25Mb1JhV0FOX1VuaXZfU2F2b2llXzIwMjMhCg==',
-                port=tunnel.local_bind_port,
-                username=influxdb_username,
-                password=influxdb_password,
-                database=influxdb_bucket,
-            )
-
-        if client:
-            print("Connexion à la base de données InfluxDB réussie.")
-            return client
-        else:
-            print("Échec de la connexion à la base de données InfluxDB.")
-            return False
-
-    except Exception as e:
-        print(f"Une erreur s'est produite : {e}")
-        return e
-
-def fermer_connexion(client):
-    if client:    
-        client.close()
-        print("Connexion à la base de données InfluxDB fermée avec succès.")
-        return True
-    else:
-        print("Aucune connexion à fermer.")
-        return False
-
-client = connexion_bdd(url, ssh_port, login, mdp, influxdb_address, influxdb_port, influxdb_username, influxdb_password, influxdb_bucket)
-fermer_connexion(client)
+client = connexion(host,token,org)
+writeData(client, bucket,org)
+readData(client,org)
+close(client)
 
 
 
